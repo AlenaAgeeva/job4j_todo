3cb9eef09bab49a59c95c6cdee5579ddfac0abc6
Alena Ageeva <alenaageevapv@gmail.com>
2024-08-25 21:06:10 -0300

    Add one.html view for editing a single task from the list, add related methods to TaskController, fill in readme file

---
+++ README.md
@@ -0,0 +1,42 @@
+# job4j_todo
+
+## Project Description
+
+A simple task management application. This application demonstrates the use of the technology stack including Spring Boot,
+Thymeleaf, Bootstrap, Hibernate, and PostgreSQL. The task list can be useful for creating tasks, viewing the list of tasks,
+modifying tasks, and tracking their status.
+
+
+## Technology Stack
+- Java 17
+- PostgreSQL
+- springframework.boot 2.7.6
+- Hibernate
+- Lombok
+- H2database
+- Liquibase
+
+
+## Running the Project
+-Create a database named 'todo' in PostgreSQL.
+-Check the database connection settings in the following files:
+--db/liquibase.properties
+--src/main/resources/hibernate.cfg.xml
+-Before starting, run the command liquibase:update.
+-Run the main method.
+-Open the browser at http://localhost:8080/.
+
+
+### All tasks
+<image src="https://github.com/AlenaAgeeva/job4j_todo/blob/master/screenshots/img.png" alt="All tasks" />
+
+### Filtered tasks
+<image src="https://github.com/AlenaAgeeva/job4j_todo/blob/master/screenshots/img_1.png" alt="Filtered tasks" />
+
+<image src="https://github.com/AlenaAgeeva/job4j_todo/blob/master/screenshots/img_2.png" alt="Filtered tasks" />
+
+### Adding a new task
+<image src="https://github.com/AlenaAgeeva/job4j_todo/blob/master/screenshots/img_3.png" alt="Adding a new task" />
+
+### Editing a task
+<image src="https://github.com/AlenaAgeeva/job4j_todo/blob/master/screenshots/img_4.png" alt="Editing a task" />
--- src/main/java/todo/controller/TaskController.java
+++ src/main/java/todo/controller/TaskController.java
@@ -2,10 +2,7 @@ package todo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import todo.model.Task;
 import todo.service.SimpleTaskService;
 
@@ -21,13 +18,13 @@ public class TaskController {
     @GetMapping("/done")
     public String getAllDone(Model model) {
         model.addAttribute("tasks", taskService.findByStatus(true));
-        return "tasks/done";
+        return "tasks/list";
     }
 
     @GetMapping("/new")
     public String getAllNew(Model model) {
         model.addAttribute("tasks", taskService.findByStatus(false));
-        return "tasks/new";
+        return "tasks/list";
     }
 
     @GetMapping("/create")
@@ -41,6 +38,32 @@ public class TaskController {
         try {
             taskService.save(task);
             model.addAttribute("tasks", taskService.findAll());
+            return "redirect:/";
+        } catch (Exception exception) {
+            model.addAttribute("message", exception.getMessage());
+            return "errors/404";
+        }
+    }
+
+    @GetMapping("/{id}")
+    public String getById(Model model, @PathVariable int id) {
+        var taskOptional = taskService.findById(id);
+        if (taskOptional.isEmpty()) {
+            model.addAttribute("message", "Task with id=" + id + " is not found");
+            return "errors/404";
+        }
+        model.addAttribute("task", taskOptional.get());
+        return "tasks/one";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute Task task, Model model) {
+        try {
+            var isUpdated = taskService.update(task);
+            if (!isUpdated) {
+                model.addAttribute("message", "Task is not found");
+                return "errors/404";
+            }
             return "redirect:/";
         } catch (Exception exception) {
             model.addAttribute("message", exception.getMessage());
